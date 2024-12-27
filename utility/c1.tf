cdfc330e31f024e8cf9a1921db86e0b160b24abf
om <awsdevopsom@gmail.com>
2024-12-27 18:06:15 +0530

    om

--- utility/c1.tf
+++ utility/c1.tf
@@ -10,7 +10,8 @@ data "aws_ec2_instance_type_offerings" "my_instance" {
 
   filter {
     name   = "location"
-    values = ["us-east-1a"]
+    # values = ["us-east-1a"]
+    values = ["us-east-1e"]
   }
 
   location_type = "availability-zone"
